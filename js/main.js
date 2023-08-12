7e0a022ee16854f499d3cdeed8c7c754b8133212
boushra-tarabishi <boushratarabishi@gmail.com>
2023-08-12 15:38:24 +0300

    java

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// let x = "<h2>hi barbie</h2> "
+// document.write(x)
+// alert("hello world")
+// document.writeln("<h1>hello world</h1>")
+// document.getElementById("mycontaint").innerHTML = "hello"
+// console.log("hello world")
+
+// let elem = document.getElementById("head")
+// alert(elem)
+// document.write(elem)
+// document.getElementById("com").innerHTML = elem;
+// console.log(elem)
+
+// {
+// var y = 10;
+// }
+// document.getElementById("com").innerHTML = y;
+
+// let x = 2;
+// let y = 64;
+// let s = "hello"
+// let z = x ** y;
+// s = 5 * 2;
+// document.getElementById("mycontaint").innerHTML = s + x
+// y = parseInt(prompt("hello..."))
+
+let x = 1;
+for (i = 0; i < 100; i++) {
+    x += i + "< br > "
+}
+document.getElementById("mycontaint").innerHTML += x;
